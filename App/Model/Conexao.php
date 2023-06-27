347c0b2b0075ddaf2d0fe7ebd5f2dd992ca420f7
Rogerio Costa <rogerioncosta1@gmail.com>
2023-06-27 00:26:23 -0300

    conexao PDO

---
+++ App/Model/Conexao.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Model;
+
+use PDO;
+
+class Conexao {
+
+	private static $instance;
+
+	public static function getConn() {
+
+		if(!isset(self::$instance)) {
+			self::$instance = new PDO('mysql:host=localhost;dbname=pdo;charset=utf8', 'root');
+		}
+
+		return self::$instance;
+	}	
+}
